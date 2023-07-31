2bed438fa0438160194f4e4550152214643164f0
jungle123000 <141036177+jungle123000@users.noreply.github.com>
2023-07-31 20:40:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxf89625faa3932ab2",
+  appsecret: "b6edf34dc5ddec1eedf3d93dfafaef53",
+  template_id: "DF4KBIySeCjq_B-pdCLY4NwvpTPX7dynsmBqHIal25c",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "长沙",
+  first_date: "2023-7-1",
 };
